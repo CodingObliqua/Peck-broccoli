02bf5eb35f56881a93264916ba61aadc6e0493d3
codingobliqua <andres.asencios10@gmail.com>
2023-08-10 18:28:48 -0700

    added inquirer.prompt to index.js

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const inquirer = require('inquirer');
+const Department = require('./lib/Department');
+const Employee = require('./lib/Employee');
+const Role = require('./lib/Role');
 const { viewAllDepartments } = require('./lib/queries');
 const connection = require('./db/connection');
 
 async function main() {
     try {
+        const action = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'what would you like to do', 
+                choices: [
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add Department',
+                    'Add Employee',
+                    'Add Role'
+                ],
+            },
+        ]);
         // Initialize inquirer prompts here 
-        
+        if (action.choice === 'Add Department') {
+            const departmentName = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'enter the name of the department:',
+                    validate: (value) => {
+                        if (value.trim() === '') {
+                            return 'Department name cannot be empty.';
+                        }
+                        return true;
+                    },
+                },
+            ]);
+            await Department.addDepartment(departmentName.name);
+            console.log('Department added successfully.');
+        }
+
         // View all departments
         const departments = await viewAllDepartments();
         console.log(departments);
